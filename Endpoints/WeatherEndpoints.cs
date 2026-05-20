09aec36f4f40d67e98f0fc603678926716a8cb74
sebastianvasquezechavarria1234 <sebasvasquez1314@gmail.com>
2026-05-20 08:43:49 -0500

    feat: reorganize folder structure and configure CORS policy

---
+++ Endpoints/WeatherEndpoints.cs
@@ -0,0 +1,29 @@
+using MiAp.Models;
+
+namespace MiAp.Endpoints;
+
+public static class WeatherEndpoints
+{
+    public static void MapWeatherEndpoints(this IEndpointRouteBuilder app)
+    {
+        var summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        app.MapGet("/weatherforecast", () =>
+        {
+            var forecast = Enumerable.Range(1, 5).Select(index =>
+                new WeatherForecast
+                (
+                    DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                    Random.Shared.Next(-20, 55),
+                    summaries[Random.Shared.Next(summaries.Length)]
+                ))
+                .ToArray();
+            return Results.Ok(forecast);
+        })
+        .WithName("GetWeatherForecast")
+        .WithOpenApi();
+    }
+}
